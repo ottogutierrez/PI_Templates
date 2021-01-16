41ad0b6890adfe1d4c01fba11b97252df072d313
Otto Gutierrez <Otto.Gutierrez@startech.com>
2021-01-15 21:11:52 -0500

    feature - can create input fields or text areas

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-const Field = ({title, text, setText, maxLen, identifier})=>{
+const Field = ({title, text, setText, maxLen, identifier, textArea})=>{
     
     const [titleLen, setTitleLen] = useState(0)
     const handleChangeText = e =>{
@@ -10,14 +10,25 @@ const Field = ({title, text, setText, maxLen, identifier})=>{
     return(
         <div className="">
             <label className="block text-gray-500 text-sm font-bold mb-2" htmlFor="title">{title}</label>
-            <input 
+            {!textArea && 
+                <input 
+                    type="text" 
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
+                    id={identifier}
+                    placeholder={title}
+                    value={text}
+                    onChange={handleChangeText}
+                />   
+            }
+            {textArea && <textArea 
                 type="text" 
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
                 id={identifier}
                 placeholder={title}
                 value={text}
                 onChange={handleChangeText}
-            />    
+            />  }
+              
             <div className={`text-sm text-right ${titleLen > {maxLen} ? "text-red-400": "text-gray-400"} px-1`}>{titleLen}/{maxLen}</div>
         </div>
         
